d6968ac766efc64638045f9fe31511f8a81595da
Stephan Grein <stephan@syntaktischer-zucker.de>
2018-10-23 01:30:07 +0200

    Adding more CLI options and adding usage to README.

--- README.md
+++ README.md
@@ -6,6 +6,15 @@ Making use of the  REST API (NeuroMorpho.org v7) to query the database.
 - Get SWC file by neuron name
 - Get SWC file by brain region
 
+## Usage:
+- `python get_swc.py` will output some help method
+- `python get_swc.py --region neocortex` will download all swc to current dir
+- `python get_swc.py --region neocortex --neurons 10` will download the first 10 neurons
+- `python get_swc.py --name cnic_001` will download the specified neuron by name
+- `python get_swc.py --index 1` will download the specified neuron by index
+
+Note region cannot specified with name or index, and either name or index can be specified.
+
 ## CI
 - OSX/Linux (Python v3.4/v2.7) [![OSX/Linux](https://travis-ci.org/NeuroBox3D/neuromorpho.svg?branch=master)](https://travis-ci.org/NeuroBox3D/neuromorpho)
 - Windows (Python v2.7 and v3.4) [![Windows](https://ci.appveyor.com/api/projects/status/j0t1orah829j2yca?svg=true)](https://ci.appveyor.com/project/stephanmg/neuromorpho)
--- get_swc.py
+++ get_swc.py
@@ -4,8 +4,10 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Access NeuroMorpho.org v7 w/ REST API")
 parser.add_argument('--region', required=False, type=str, help="Brain region", metavar="R")
-parser.add_argument('--neurons', required=False, type=int, help="Number of neurons per page", metavar="N")
+parser.add_argument('--neurons', required=False, type=int, help="Count of neurons per page", metavar="C")
 parser.add_argument('--pages', required=False, type=int, help="Number of pages of neurons", metavar="P")
+parser.add_argument('--name', required=False, type=str, help="Name of neuron", metavar="N")
+parser.add_argument('--index', required=False, type=int, help="Index of neuron", metavar="I")
 args = parser.parse_args()
 
 if (args.region):
@@ -13,5 +15,10 @@ if (args.region):
   numPages = (args.pages != -1 and args.neurons) or -1
   brainRegion = (args.region != -1 and args.region) or "neocortex"
   get_swc_by_brain_region(brainRegion, numNeurons, numPages)
+if (not args.region and ((args.index != None) ^ (args.name != None))):
+  if (args.index):
+      get_swc_by_neuron_index(args.index)
+  if (args.name):
+      get_swc_by_neuron_name(args.name)
 else:
   parser.print_help()
