58067a5fb13caab9172531422be60407d69595f7
stephan <stephan@syntaktischer-zucker.de>
2020-09-03 18:42:33 +0200

    Fix for authentication.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='neuromorpho',
-     version='0.0.1',
+     version='0.0.1-a0',
      scripts=['neuromorpho'] ,
      author="Stephan Grein",
      author_email="stephan@syntaktischer-zucker.de",
