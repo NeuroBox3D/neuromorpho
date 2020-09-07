749bbd9bb416756c1ee89cb1e4177d8767a53b1b
Stephan Grein <stephan@syntaktischer-zucker.de>
2020-09-07 11:40:57 +0200

    Fix.

--- tests/test_rest_wrapper.py
+++ tests/test_rest_wrapper.py
@@ -27,7 +27,7 @@ class TestRestWrapper(unittest.TestCase):
     self.assertTrue(num_lines == 494, "SWC file incomplete!")
 
   def test_get_swc_by_filter_and_search_term(self):
-    from rest_wrapper.rest_wrapper import get_swc_by_filter_rule_and_search_term
+    from rest_wrapper.rest_wrapper import get_swc_by_filter_rule_for_search_term
     valid_filename = "H16-03-002-01-03-03_559391969_m"
     filename = get_swc_by_filter_rule_for_search_term([["cell_type=pyramidal"], \
                ["archive=Allen Cell Types"]], "brain_region:neocortex", -1, 1)
