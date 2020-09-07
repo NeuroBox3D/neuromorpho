c803ba989bd8a886e9590bf83f804fd140de4d16
Stephan Grein <stephan@syntaktischer-zucker.de>
2020-09-07 11:24:24 +0200

    Excluding externals and test packages.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
      long_description="This PyPi project allows users to query the NeuroMorpho.org database via REST request conveniently from the command line with Python2 or Python3 and integrates thus readily into a batch workflow. Database can be queried for cell names and neurons are downloaded in the SWC format.",
      long_description_content_type="text/markdown",
      url="https://github.com/stephanmg/neuromorpho",
-     packages=setuptools.find_packages(),
+     packages=setuptools.find_packages(exclude=['tests', 'externals']),
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
