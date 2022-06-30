e302f3d3593d8b93a10dba66f1b6e0c6965c8018
stephanmg <stephan@syntaktischer-zucker.de>
2022-06-30 10:06:38 +0200

    Minor release v0.0.2.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='neuromorpho',
-     version='0.0.1b',
+     version='0.0.1',
      scripts=['neuromorpho'] ,
      author="Stephan Grein",
      author_email="stephan@syntaktischer-zucker.de",
